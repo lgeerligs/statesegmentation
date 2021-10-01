d259dc2e3fe4ffab58cc5e2a811f3c1ed3480ad3
Umut Güçlü <4326568+umuguc@users.noreply.github.com>
2021-10-01 13:19:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='statesegmentation',
-    version='0.0.1',
+    version='0.0.4',
     packages=['statesegmentation'],
     url='https://github.com/lgeerligs/statesegmentation',
     license='MIT',
